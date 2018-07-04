63788dd66a4a81daa653a8b3e5aa5cd9c68dad99
Igor Blinnikov <Igor.Blinnikov@Broadridge.com>
2018-07-04 13:06:01 +0300

    feat(hybrid app): rename app module to ng1app feat(hybrid app): move ng1 sources to /ng1-src folder

--- ng1-src/modules/app/app.module.js
+++ ng1-src/modules/app/app.module.js
@@ -2,9 +2,8 @@ import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 
 import '@progress/kendo-ui/js/kendo.angular';
-
 import '@progress/kendo-ui/css/web/kendo.bootstrap-v4.min.css';
 
-const app = angular.module("app", ['auth', uiRouter, "kendo.directives", 'templates']);
+const app = angular.module("ng1app", ['auth', uiRouter, "kendo.directives", 'templates']);
 
 export default app;
