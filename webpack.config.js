74964ad16e6441b26f8228a1f28354d0d8497692
Igor Blinnikov <Igor.Blinnikov@Broadridge.com>
2018-07-04 13:08:59 +0300

    feat(hybrid app): tweak webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const { AngularCompilerPlugin } = require('@ngtools/webpack');
+
 const path = require('path');
 const webpack = require('webpack');
 
 const entries = ['app', 'auth'].reduce((result, moduleName) => {
-    result[`${moduleName}`] = `./modules/${moduleName}/${moduleName}.config.js`;
+    result[`${moduleName}`] = `./ng1-src/modules/${moduleName}/${moduleName}.config.js`;
     return result;
 }, {});
+
+entries.main = ["./ng2-src\\main.ts"];
+entries.polyfills = ["./ng2-src\\polyfills.ts"];
+entries.styles = ["./ng2-src\\styles.css"];
 
 module.exports = {
     entry: entries,
@@ -15,10 +21,27 @@ module.exports = {
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
     mode: "development",
+    optimization: {
+       splitChunks: {
+           chunks: 'all',
+           name: 'vendor'
+       },
+    },
     plugins: [
         new webpack.ProvidePlugin({
             'window.jQuery': 'jquery'
-        })
+        }),
+        new AngularCompilerPlugin({
+            "mainPath": "main.ts",
+            "platform": 0,
+            "hostReplacementPaths": {
+              "environments\\environment.ts": "environments\\environment.ts"
+            },
+            "sourceMap": true,
+            "tsConfigPath": "ng2-src\\tsconfig.app.json",
+            "skipCodeGeneration": true,
+            "compilerOptions": {}
+          })
     ],
     module: {
         rules: [
@@ -26,8 +49,39 @@ module.exports = {
             {
                 enforce: 'pre',
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                loader: "style-loader!css-loader",
+                "exclude": [
+                    path.join(process.cwd(), "ng2-src\\**")
+                ],
             },
+
+            {
+                "include": [
+                    path.join(process.cwd(), "ng2-src\\app\\app.component.css")
+                ],
+                // "exclude": [
+                //   path.join(process.cwd(), "ng2-src\\styles.css")
+                // ],
+                "test": /\.css$/,
+                "use": [
+                  {
+                      "loader": "raw-loader",
+                    //   "loader": "style-loader"
+                }
+                ]
+              },
+              {
+                "include": [
+                  path.join(process.cwd(), "ng2-src\\styles.css")
+                ],
+                "test": /\.css$/,
+                "use": [
+                  "style-loader",
+                  {
+                    "loader": "raw-loader"
+                  }
+                ]
+              },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
@@ -36,19 +90,26 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             },
-            // {
-            //     enforce: 'pre',
-            //     test: /\.js$/,
-            //     use: {
-            //         loader: 'source-map-loader'
-            //     }
-            // },
+            {
+                "test": /\.html$/,
+                "exclude": [
+                    path.join(process.cwd(), "ng1-src")
+                ],
+                "loader": "raw-loader"
+            },
             {
                 test: /\.html$/,
+                "exclude": [
+                    path.join(process.cwd(), "ng2-src")
+                ],
                 use: {
                     loader: 'ng-cache-loader?module=templates'
                 }
             },
+            {
+                test: /\.ts$/,
+                loader: "@ngtools/webpack"
+            }
         ]
     }
 };
